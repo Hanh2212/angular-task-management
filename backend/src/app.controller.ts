eb0b56a22f89e59d2468005480ec1137707bb5f1
HanhDo <dthhanhptit@gmail.com>
2022-03-16 09:36:40 +0700

    update: crud_api

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Redirect, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Redirect('http://localhost:6789/login')
   getHello(): string {
     return this.appService.getHello();
   }
--- backend/src/list/list.schema.ts
+++ backend/src/list/list.schema.ts
@@ -4,8 +4,7 @@ export type ListDocument = ListTask & Document;
 
 @Schema()
 export class ListTask {
-    @Prop({required: true, minLength: 4, trim: true})
-    title: string;
+    @Prop({required: true, minLength: 4, trim: true}) title: string;
 }
 
 export const ListSchema = SchemaFactory.createForClass(ListTask);
