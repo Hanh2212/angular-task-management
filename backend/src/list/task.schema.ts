1b6ae59beb2015e141cdadb4496b605a9ca253c7
HanhDo <dthhanhptit@gmail.com>
2022-03-21 17:09:41 +0700

    add: unique field

--- backend/src/list/task.schema.ts
+++ backend/src/list/task.schema.ts
@@ -6,7 +6,7 @@ export type TaskDocument = Task & Document;
 @Schema()
 export class Task {
     @Prop({required: true}) _listId: mongoose.Schema.Types.ObjectId;
-    @Prop({required: true, minLength: 4, trim: true}) title: string;
+    @Prop({unique: true, required: true, minLength: 4, trim: true}) title: string;
     @Prop({required: true, minLength: 4, trim: true}) status: string;
     @Prop({minLength: 5, trim: true}) description: string;
 }
